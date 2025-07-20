936779105778ad3d8a4df1dd149d8d337e5acd59
kristinka-skl <kristinka.skl@gmail.com>
2025-07-20 18:14:25 +0200

    ready

--- js/task-3.js
+++
@@ -1,7 +0,0 @@
-const inputElem = document.querySelector('#name-input');
-const nameElem = document.querySelector('#name-output');
-inputElem.addEventListener('input', inputElemHandler);
-function inputElemHandler() {    
-    const username = inputElem.value.trim();  
-    nameElem.textContent = username === '' ? 'Anonymous': username;    
-};
